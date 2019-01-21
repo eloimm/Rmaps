6d64eeb20af0369ee809163d1f87e46472fcd4f3
eloimm <6960257+eloimm@users.noreply.github.com>
2019-01-21 22:09:36 +0100

    prova
    prova

--- script.R
+++ script.R
@@ -16,8 +16,10 @@ poligons@data$nom_comar[poligons@data$nom_comar=="Val d'Aran"] <- "Aran"
 poligons@data$nom_comar <- sort(poligons@data$nom_comar)
 poligons@data$nom_comar <- as.factor(poligons@data$nom_comar)
 
+
 # Cargamos los datos que queremos representar
-load(file = "data/dades.Rdata")
+load(file = "data/dades.RData")
+
 
 ### Creamos una columna en los datos comun con los polígonos
 dades$id <- poligons@data$comarca
@@ -26,42 +28,65 @@ dades$id <- poligons@data$comarca
 # Ahora podemos juntos los dos objetos
 mapa <- merge(poligons, dades, by.x = "comarca", by.y= "id")
 
-
+# Dibujamos el mapa
 library(tmap)
-tm_shape(mapa) + 
-  tm_fill("2012", breaks = c(50, 100,150, 200, 250, 300, 350, 400, 450,500), palette = "-RdYlGn", style = "fixed")+
+mp1 <- tm_shape(mapa) + 
+  tm_fill("2012", # Columna a pintar
+          palette = "-RdYlGn")+
+  tm_layout(frame = FALSE)+
+  tm_style("white")
+
+mp1
+tmap_save(mp1, filename = "images/2012.png")
+
+# ...però cuando queremos comparar este mapa con el del año 2014...
+# Creamos el mapa 2014
+mp2 <- tm_shape(mapa) + 
+    tm_fill("2014", # Columna a pintar
+          palette = "-RdYlGn")+
+  tm_layout(frame = FALSE)+
+tmap_save(mp2, filename = "images/2014.png")
+
+# Los ploteamos uno junto al lado del otro
+library(grid)
+library(gridExtra)
+library(ggplot2)
+
+png(filename = "images/Mp1Mp2.png")
+grid.newpage()
+pushViewport(viewport(layout=grid.layout(1,2)))
+print(mp1, vp=viewport(layout.pos.col = 1, layout.pos.row =1))
+print(mp2, vp=viewport(layout.pos.col = 2, layout.pos.row =1))
+dev.off()
+
+# Los valores y colores de la escala nos distorcionan poder comparar los mapas. 
+#Asignamos los rangos y *fijamos* el estilo para cada año
+
+map1 <- tm_shape(mapa) + 
+  tm_fill("2012", 
+          breaks = c(25, 50, 75, 100, 125, 150, 175, 200), palette = "-RdYlGn", style = "fixed")+
   tm_layout(legend.show = FALSE, frame = FALSE)
 
 map2 <-tm_shape(mapa) + 
-  tm_fill("2013", breaks = c(50, 100,150, 200, 250, 300, 350, 400, 450,500), palette = "-RdYlGn", style = "fixed")+
+  tm_fill("2013", breaks = c(25, 50, 75, 100, 125, 150, 175, 200), palette = "-RdYlGn", style = "fixed")+
   tm_layout(legend.show = FALSE, frame = FALSE)
 
-map3 <-tm_shape(mapa) + 
-  tm_fill("2014", breaks = c(50, 100,150, 200, 250, 300, 350, 400, 450,500), palette = "-RdYlGn", style = "fixed")+
-  tm_layout(legend.show = FALSE, frame = FALSE)
-
-map4 <-tm_shape(mapa) + 
-  tm_fill("2015", breaks = c(50, 100,150, 200, 250, 300, 350, 400, 450,500), palette = "-RdYlGn", style = "fixed")+
-  tm_layout(legend.show = FALSE, frame = FALSE)
-
-tm_shape(mapa) + 
-  tm_fill("2016", breaks = c(50, 100,150, 200, 250, 300, 350, 400, 450,500), palette = "-RdYlGn", style = "fixed", title)+
-  tm_layout(title = "Expedients x 10.000 habitants",frame = FALSE, legend.outside = TRUE)+
-  tm_legend(... = FALSE)
-
-# 
+map3 <- tm_shape(mapa) + 
+  tm_fill("2014", breaks = c(25, 50, 75, 100, 125, 150, 175, 200), palette = "-RdYlGn", style = "fixed")+
+  tm_layout(legend.show = FALSE, frame = FALSE, legend.outside=TRUE, legend.outside.position="bottom")
 
 
-library(grid)
-library(gridExtra)
+
+
+
+png(filename = "images/Map1Map2Map3.png")
 grid.newpage()
 pushViewport(viewport(layout=grid.layout(2,3)))
 print(map1, vp=viewport(layout.pos.col = 1, layout.pos.row =1))
 print(map2, vp=viewport(layout.pos.col = 2, layout.pos.row =1))
 print(map3, vp=viewport(layout.pos.col = 3, layout.pos.row =1))
-print(map4, vp=viewport(layout.pos.col = 1, layout.pos.row =2))
-print(map5, vp=viewport(layout.pos.col = 2, layout.pos.row =2))
 
+dev.off()
 
 ### PER A LA LLEGENDA
 g_legend<-function(a.gplot){
